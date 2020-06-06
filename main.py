21b9dbc5f48347008d051c28790e8c38a61fabdf
ImSurfinBird <64488547+ImSurfinBird@users.noreply.github.com>
2020-06-06 04:04:52 +0200

    Updated main logic

--- main.py
+++ main.py
@@ -15,7 +15,24 @@ import pygame as pg
 
 
 class Suffer:
-    def destroy_object(self, object_data):
+    def duplicate_object(self, object_name):
+        self.active[self.key] = self.objects[object_name].copy()
+        self.key += 1
+
+    def move(self, direction, object_data):
+        self.remove_object(object_data)
+        if direction == 'UP':
+            object_data['y'] -= object_data['speed']
+        if direction == 'DOWN':
+            object_data['y'] += object_data['speed']
+        if direction == 'LEFT':
+            object_data['x'] -= object_data['speed']
+        if direction == 'RIGHT':
+            object_data['x'] += object_data['speed']
+        self.create_object(object_data)
+        return object_data
+
+    def remove_object(self, object_data):
         pg.draw.rect(self.win, self.bg, (object_data['x'], object_data['y'], object_data['width'], object_data['height']))
         pg.display.update()
 
@@ -30,15 +47,31 @@ class Suffer:
             'width': 50,
             'height': 50,
             'color': (255, 0, 0),
-            'x': 0,
-            'y': 0,
+            'x': 50,
+            'y': 50,
             'displayed': True,
             'drag': True,
             'dragging': False,
+            'moving': False,
+            'type': "Rectangle",
+        }
+        enemy = {
+            'name': "enemy",
+            'width': 50,
+            'height': 50,
+            'color': (0, 255, 0),
+            'x': 0,
+            'y': 0,
+            'displayed': False,
+            'drag': False,
+            'dragging': False,
+            'speed': 5,
+            'moving': True,
             'type': "Rectangle",
         }
         self.objects = {
             object1['name']: object1,
+            enemy['name']: enemy,
         }
         for x in self.objects:
             if self.objects[x]['displayed']:
@@ -49,7 +82,7 @@ class Suffer:
             object_data['dragging'] = True
         if self.m[0] == 0 and object_data['dragging']:
             object_data['dragging'] = False
-            self.destroy_object(object_data)
+            self.remove_object(object_data)
             object_data['x'] = self.p[0] - int(object_data['width'] / 2)
             object_data['y'] = self.p[1] - int(object_data['height'] / 2)
             self.create_object(object_data)
@@ -58,9 +91,12 @@ class Suffer:
     def __init__(self):
         #Initialize background variables:
         self.bg = (0, 0, 0)
-        self.window_size = (500, 500)
+        self.window_size = (1910, 1040)
         self.drags = {}
         run = True
+        self.counter = -1
+        self.key = 0
+        self.active = {}
 
         pg.init()
         self.win = pg.display.set_mode(self.window_size)
@@ -77,11 +113,17 @@ class Suffer:
 
             self.m = pg.mouse.get_pressed()
             self.p = pg.mouse.get_pos()
+            self.counter += 1
+
+            if self.counter % 100 == 0:
+                self.duplicate_object('enemy')
 
             for x in self.objects:
                 if self.objects[x]['drag']:
                     self.objects[x] = self.drag_and_drop(self.objects[x])
-
+            for x in self.active:
+                if self.active[x]['moving']:
+                    self.active[x] = self.move('RIGHT', self.active[x])
 
 
 Suffer()
