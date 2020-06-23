947e3abe4d4b38b63e9f0b8fdac8daf50c405e1c
ImSurfinBird <64488547+ImSurfinBird@users.noreply.github.com>
2020-06-23 22:21:40 +0200

    Added basic jumping logic

--- PyGame/ObjectFunctions.py
+++ PyGame/ObjectFunctions.py
@@ -6,39 +6,3 @@ def drag_and_drop(mouse_button1, mouse_coords, object_data):
         object_data['x'] = mouse_coords[0] - int(object_data['width'] / 2)
         object_data['y'] = mouse_coords[1] - int(object_data['height'] / 2)
     return object_data
-
-
-def move_path(object_data):
-    for s in range(object_data['speed']):
-        if(object_data['x'], object_data['y']) in object_data['path'].keys():
-            object_data['direction'] = object_data['path'][(object_data['x'], object_data['y'])]
-        if object_data['direction'] == 'UP':
-            object_data['y'] -= 1
-        if object_data['direction'] == 'DOWN':
-            object_data['y'] += 1
-        if object_data['direction'] == 'LEFT':
-            object_data['x'] -= 1
-        if object_data['direction'] == 'RIGHT':
-            object_data['x'] += 1
-    return object_data
-
-
-def move(object_data):
-    if object_data['direction'] == 'UP':
-        object_data['y'] -= object_data['speed']
-    if object_data['direction'] == 'DOWN':
-        object_data['y'] += object_data['speed']
-    if object_data['direction'] == 'LEFT':
-        object_data['x'] -= object_data['speed']
-    if object_data['direction'] == 'RIGHT':
-        object_data['x'] += object_data['speed']
-    return object_data
-
-
-def jump(space_bar, object_data):
-    if object_data['jumping'] == 0 and space_bar == 1:
-        object_data['jumping'] += 1
-    if object_data['jumping'] > 0:
-        object_data['y'] -= int((object_data['jump_velocity'] - pow(object_data['jumping'], 2))/10)
-        object_data['jumping'] += 1
-    return object_data
--- PyGame/main_2.py
+++ PyGame/main_2.py
@@ -26,12 +26,6 @@ class Suffer:
         self.key = 0
         self.active = {}
         self.objects = {}
-        path = {
-            (0, 0): 'RIGHT',
-            (150, 0): 'DOWN',
-            (150, 200): 'LEFT',
-            (0, 200): 'UP',
-        }
 
         pg.init()
         self.win = pg.display.set_mode(self.window_size)
@@ -41,11 +35,6 @@ class Suffer:
 
         self.objects = obj.create_object(self.objects, "Test", (0, 0), "moon2SUFFER.png")
         self.objects['Test']['drag'] = False
-        #self.objects['Test']['path'] = path
-        #self.objects['Test']['speed'] = 5
-        #self.objects['Test']['direction'] = 'RIGHT'
-        self.objects['Test']['jumping'] = 0
-        self.objects['Test']['jump_velocity'] = 250
 
         self.active['Test'] = obj.duplicate_object('Test', self.objects)
         obj.display_object(self.win, 'Test', self.active)
@@ -60,20 +49,10 @@ class Suffer:
 
             self.m = pg.mouse.get_pressed()
             self.p = pg.mouse.get_pos()
-            self.space = pg.key.get_pressed()[32]
-            self.counter += 1
 
             for x in self.active:
                 if self.active[x].get('drag') != None:
                     self.active[x] = fun.drag_and_drop(self.m, self.p, self.active[x])
-                if self.active[x].get('path') != None:
-                    self.active[x] = fun.move_path(self.active[x])
-                if self.active[x].get('speed') != None and self.active[x].get('path') == None:
-                    self.active[x] = fun.move(self.active[x])
-                if self.active[x].get('jumping') != None:
-                    self.active[x] = fun.jump(self.space, self.active[x])
-                if self.active[x].get('y') >= 600:
-                    self.active[x]['jumping'] = 0
 
             obj.update_display(self.win, self.bg, self.window_size, self.active)
 
